1f7c897b920747a15709bbe698d78f5bb1dd68d9
RioPisces2001 <riopisces.pqh@gmail.com>
2024-08-02 20:45:56 +0700

    backup before reset

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ActivatedRoute, RouterModule } from '@angular/router';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/partials/header/header.component';
 import { HomeComponent } from './components/pages/home/home.component';
@@ -12,6 +12,7 @@ import { FoodPageComponent } from './components/pages/food-page/food-page.compon
 import { CartPageComponent } from './components/pages/cart-page/cart-page.component';
 import { TitleComponent } from './components/partials/title/title.component';
 import { NotFoundComponent } from './components/partials/not-found/not-found.component';
+import { LoginPageComponent } from './components/pages/login-page/login-page.component';
 
 @NgModule({
   declarations: [
@@ -23,13 +24,14 @@ import { NotFoundComponent } from './components/partials/not-found/not-found.com
     FoodPageComponent,
     CartPageComponent,
     TitleComponent,
-    NotFoundComponent
+    NotFoundComponent,
+    LoginPageComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     RouterModule.forRoot([
-
     ])
   ],
   providers: [],
--- frontend/src/app/components/pages/home/home.component.ts
+++ frontend/src/app/components/pages/home/home.component.ts
@@ -1,22 +1,31 @@
 import { ActivatedRoute } from '@angular/router';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FoodService } from 'src/app/services/food.service';
 import { Food } from 'src/app/shared/models/Food';
+import { Observable } from 'rxjs';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
   foods:Food[] = [];
   constructor(private foodService:FoodService,activatedRoute:ActivatedRoute) {
+    let foodsObservable:Observable<Food[]> ;
     activatedRoute.params.subscribe((params)=>{
       if(params.searchTerm)
-        this.foods = this.foodService.getAllFoodBySearchTerm(params.searchTerm);
+        foodsObservable = this.foodService.getAllFoodBySearchTerm(params.searchTerm);
       else if(params.tag)
-        this.foods = this.foodService.getAllFoodByTag(params.tag);
+        foodsObservable = this.foodService.getAllFoodByTag(params.tag);
       else
-        this.foods = this.foodService.getAll();
+        foodsObservable = foodService.getAll();
+
+        foodsObservable.subscribe((serverFood) => {
+          this.foods = serverFood;});
     })
+  }
+
+  ngOnInit():void{
+
   }
 }
